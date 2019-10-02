fcea89eedb059966d3a29108fdec2da0e06362a8
idzharbae <idzharbae@gmail.com>
2019-10-02 17:23:40 +0700

    fix docker

---
+++ web/hello-js-2/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	let query;
+	if(!req.query.q){
+		res.redirect(req.url+'?q=undefined');
+	}else{
+		query = req.query.q;
+	}
+  res.send('<html><head><title>Hello JS</title></head><body>Hello ' + eval('\"'+query+'!\"') +' Welcome to level 2</body></html>' );
+  
+});
+
+app.listen(50050, function () {
+  console.log('Example app listening on port 50050!');
+});
